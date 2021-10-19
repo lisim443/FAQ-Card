c11af9a509b7de84da649c951530f808255078d0
lisim <lior443@gmail.com>
2021-10-19 19:11:53 +0300

    first draft of FAQ card, added rough draft for HTML/CSS, Javascript code still in progress.

---
+++ script.js
@@ -0,0 +1,34 @@
+const questions = document.getElementsByClassName("questions");
+const answers = document.getElementsByClassName("answers");
+
+answers.style.visibility = "hidden";
+
+// function toggle() {
+//   answers.style.visibility = answers.style.visibility = "visbile"
+//     ? "hidden"
+//     : "visible";
+// }
+
+// questions.onClick = () => {
+//     for (i = 0; i < coll.length; i++) {
+//         coll[i].addEventListener("click", function() {
+//           this.classList.toggle("active");
+//           var content = this.nextElementSibling;
+//           if (content.style.display === "block") {
+//             content.style.display = "none";
+//           } else {
+//             content.style.display = "block";
+//           }
+//         });
+//       }
+// }
+
+// questions.forEach = (question) => {
+//   question.addEventListener("click", function (event) {
+//     if (event.style.visibility === "visible") {
+//       event.style.visibility = "hidden";
+//     } else {
+//       event.style.visibility = "visible";
+//     }
+//   });
+// };
